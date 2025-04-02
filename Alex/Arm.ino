7753c379d781d9c8f45854fbe4e41deb43df9186
Wenbo (Daniel) Zhu <daniel.wb.zhu@gmail.com>
2025-04-02 15:09:28 +0800

    Update ARM

--- Alex/Arm.ino
+++ Alex/Arm.ino
@@ -1,14 +1,13 @@
 #include <Servo.h>
-Servo leftArm;
 Servo rightArm;
+Servo leftArm;
 
 void setupArm() {
-  leftArm.attach(16);
-  rightArm.attach(15);
+  rightArm.attach(16);
+  leftArm.attach(15);
 
-  // Set initial positions
-  leftArm.write(0);    // Set left servo to 0 degrees
-  rightArm.write(90);  // Set right servo to 90 degrees
+  rightArm.write(90);
+  leftArm.write(0);
 }
 
 // Process color readings and control servos
@@ -16,35 +15,35 @@ void processColor(TColorType colorType) {
   // Determine color based on RGB frequencies
   switch (colorType) {
     case WHITE:
-      leftArm.write(90); // Open mechanism
-      rightArm.write(0);
+      rightArm.write(90); // Open mechanism
+      leftArm.write(0);
       break;
       
     case RED:
       if (redFreq < blueFreq && redFreq <= greenFreq && redFreq < 23) {
-        leftArm.write(45); // Close mechanism
-        rightArm.write(45);
+        rightArm.write(45); // Close mechanism
+        leftArm.write(45);
       }
       break;
       
     case BLUE:
       if (blueFreq < greenFreq && blueFreq < redFreq && blueFreq < 20) {
-        leftArm.write(90); // Open mechanism
-        rightArm.write(0);
+        rightArm.write(90); // Open mechanism
+        leftArm.write(0);
       }
       break;
       
     case GREEN:
       if (greenFreq < redFreq && (greenFreq - blueFreq) <= 8) {
-        leftArm.write(90); // Open mechanism
-        rightArm.write(0);
+        rightArm.write(90); // Open mechanism
+        leftArm.write(0);
       }
       break;
       
     case UNKNOWN:
     default:
-      leftArm.write(90); // Open mechanism
-      rightArm.write(0);
+      rightArm.write(90); // Open mechanism
+      leftArm.write(0);
       break;
   }
 }
